780ebea337c08cb5476cbfdda5af4183f5c53e5e
Kareem Virani <57181972+kareemv@users.noreply.github.com>
2024-07-14 00:57:48 -0400

    Update wallet interface

--- src/main/java/com/github/kareemv/titan/wallet/Wallet.java
+++ src/main/java/com/github/kareemv/titan/wallet/Wallet.java
@@ -1,11 +1,20 @@
 package com.github.kareemv.titan.wallet;
 
+import com.github.kareemv.titan.wallet.exception.BalanceUpdateException;
 import java.math.BigDecimal;
 
 public interface Wallet {
-    String getName();
-    void setName(String name);
-    String getAddress();
-    WalletType getWalletType();
-    BigDecimal getDisplayBalance();
+  String getName();
+
+  void setName(String name);
+
+  String getAddress();
+
+  WalletType getWalletType();
+
+  BigDecimal getDisplayBalance();
+
+  String getDisplayBalanceCurrency();
+
+  void updateBalance() throws BalanceUpdateException;
 }
